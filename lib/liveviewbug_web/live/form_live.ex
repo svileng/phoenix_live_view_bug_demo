d30187a43526313a2b830646c2cb713fed7067ea
Svilen Gospodinov <svilen.gospodinov@gmail.com>
2020-07-06 22:32:03 +0100

    Bug demo

--- lib/liveviewbug_web/live/form_live.ex
+++ lib/liveviewbug_web/live/form_live.ex
@@ -2,10 +2,30 @@ defmodule LiveviewbugWeb.FormLive do
   use LiveviewbugWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :bar, "")}
   end
 
   def render(assigns) do
     LiveviewbugWeb.LayoutView.render("form.html", assigns)
   end
+
+  def handle_event("changed", %{"_target" => ["bug", "foo"], "bug" => params}, socket) do
+    socket =
+      assign(socket, [
+        bar: params["foo"]
+      ])
+
+    {:noreply, socket}
+  end
+
+  def handle_event("changed", %{"_target" => ["bug", "bar"], "bug" => params}, socket) do
+    socket =
+      assign(socket, [
+        bar: params["bar"]
+      ])
+
+    {:noreply, socket}
+  end
+
+  def handle_event("changed", _params, socket), do: {:noreply, socket}
 end
