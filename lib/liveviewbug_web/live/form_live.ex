f83fc1885745a0f85b980942e59925ffd80ec538
Svilen Gospodinov <svilen.gospodinov@gmail.com>
2020-07-07 20:02:50 +0100

    Second attempt

--- lib/liveviewbug_web/live/form_live.ex
+++ lib/liveviewbug_web/live/form_live.ex
@@ -2,7 +2,7 @@ defmodule LiveviewbugWeb.FormLive do
   use LiveviewbugWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :bar, "")}
+    {:ok, assign(socket, foo: "", bar: "", flag: true)}
   end
 
   def render(assigns) do
@@ -10,21 +10,14 @@ defmodule LiveviewbugWeb.FormLive do
   end
 
   def handle_event("changed", %{"_target" => ["bug", "foo"], "bug" => params}, socket) do
-    socket =
-      assign(socket, [
-        bar: params["foo"]
-      ])
-
+    socket = assign(socket, foo: params["foo"], bar: params["foo"])
     {:noreply, socket}
   end
 
-  def handle_event("changed", %{"_target" => ["bug", "bar"], "bug" => params}, socket) do
-    socket =
-      assign(socket, [
-        bar: params["bar"]
-      ])
-
-    {:noreply, socket}
+  def handle_event("changed", %{"_target" => ["bug", "bar"]}, socket) do
+    # This assignment is not related to the form but will
+    # cause the `foo` field to go blank, while the `bar` field will be okay.
+    {:noreply, assign(socket, flag: false)}
   end
 
   def handle_event("changed", _params, socket), do: {:noreply, socket}
